898675152510c97fdfc5b55a78a585e135fc82b1
李成龙 <lichenglong004@ke.com>
2020-07-17 00:09:58 +0800

    报告增加接口性能数据

--- pages/report/detail.vue
+++ pages/report/detail.vue
@@ -7,7 +7,7 @@
     </el-row>
     <el-row>
       <div class="title">
-        Summary
+        汇总
       </div>
     </el-row>
     <el-row>
@@ -45,7 +45,7 @@
     </el-row>
     <el-row>
       <div class="title">
-        Detail
+        断言
       </div>
     </el-row>
     <el-row>
@@ -57,38 +57,33 @@
       >
         <el-table-column
           prop="name"
-          label="用例名"
+          label="接口名"
           width="200"
         />
         <el-table-column
           prop="start"
           label="开始时间"
-          width="180"
+          width="200"
         />
         <el-table-column
           prop="end"
           label="结束时间"
-          width="180"
-        />
-        <el-table-column
-          prop="elapsed"
-          label="请求时间(ms)"
-          width="120"
+          width="200"
         />
         <el-table-column
           prop="operate"
           label="比较操作"
-          width="180"
+          width="200"
         />
         <el-table-column
           prop="expect"
           label="期盼结果"
-          width="180"
+          width="200"
         />
         <el-table-column
           prop="actual"
           label="实际结果"
-          width="180"
+          width="200"
         />
         <el-table-column
           prop="status"
@@ -100,6 +95,58 @@
               disable-transitions
             >
               {{ scope.row.status }}
+            </el-tag>
+          </template>
+        </el-table-column>
+      </el-table>
+    </el-row>
+    <el-row>
+      <div class="title">
+        性能
+      </div>
+    </el-row>
+    <el-row>
+      <el-table
+        :data="performance"
+        :row-class-name="tableRowClassName"
+        style="width: 100%"
+        border
+      >
+        <el-table-column
+          prop="name"
+          label="接口名"
+          width="200"
+        />
+        <el-table-column
+          prop="start"
+          label="开始时间"
+          width="200"
+        />
+        <el-table-column
+          prop="end"
+          label="结束时间"
+          width="200"
+        />
+        <el-table-column
+          prop="elapsed"
+          label="接口耗时"
+          width="200"
+        />
+        <el-table-column
+          prop="threshold"
+          label="时间基准"
+          width="200"
+        />
+        <el-table-column
+          prop="performance"
+          label="状态"
+        >
+          <template slot-scope="scope">
+            <el-tag
+              :type="scope.row.status <= 1.0 ? 'success' : 'danger'"
+              disable-transitions
+            >
+              {{ scope.row.performance + '%' }}
             </el-tag>
           </template>
         </el-table-column>
@@ -119,6 +166,7 @@ export default {
       },
       headers: [],
       results: [],
+      performance: [],
       total: 0,
       success: 0,
       failed: 0,
@@ -132,13 +180,6 @@ export default {
     this.refresh()
   },
   methods: {
-    handleFormat (data) {
-      // python返回的None数据通过json转换成js的null。
-      if (data === null) {
-        return 'None'
-      }
-      return data
-    },
     refresh () {
       this.$axios
         .post('/api/v1/report/search', { id: this.$route.query.id })
@@ -154,23 +195,28 @@ export default {
             // 这里是报告的详细数据。
             for (const name in this.data.detail) {
               for (const index in this.data.detail[name].result) {
-                let elapsed = 0
-                elapsed = this.handleFormat(this.data.detail[name].elapsed)
-                let actual = '-'
-                actual = this.handleFormat(this.data.detail[name].result[index].actual)
-                let expect = '-'
-                expect = this.handleFormat(this.data.detail[name].result[index].expect)
+                const actual = this.data.detail[name].result[index].actual
                 this.results.push({
                   name: this.data.detail[name].name,
                   start: this.data.detail[name].start,
                   end: this.data.detail[name].end,
-                  elapsed,
-                  status: this.data.detail[name].result[index].status,
-                  actual,
-                  expect,
-                  operate: this.data.detail[name].result[index].operate || 'None'
+                  operate: this.data.detail[name].result[index].operate || '-',
+                  expect: this.data.detail[name].result[index].expect || '-',
+                  actual: actual !== null ? actual : 'None',
+                  status: this.data.detail[name].result[index].status
                 })
               }
+            }
+            // 这里是报告的性能数据。
+            for (const name in this.data.detail) {
+              this.performance.push({
+                name: this.data.detail[name].name,
+                start: this.data.detail[name].start,
+                end: this.data.detail[name].end,
+                elapsed: this.data.detail[name].elapsed || 0,
+                threshold: this.data.detail[name].threshold,
+                performance: this.data.detail[name].performance
+              })
             }
             // 这里是报告的表头数据。
             this.headers.push({
